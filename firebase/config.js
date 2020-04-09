89a323ea11ccf189cc4c4f4a6a02607fb358c944
David Kemp <davkem43@gmail.com>
2020-04-08 20:19:15 -0500

    fixes to env for Netlify3

--- firebase/config.js
+++ firebase/config.js
@@ -1,11 +1,9 @@
-import "./../env";
-
 export default {
-  apiKey: process.env.FIREBASE_API,
+  apiKey: "AIzaSyDoA3fPaOOpkwOY4UVsOgSIlEXSnTt_Wyk",
   authDomain: "david-s-project-f6d5c.firebaseapp.com",
   databaseURL: "https://david-s-project-f6d5c.firebaseio.com",
   projectId: "david-s-project-f6d5c",
   storageBucket: "david-s-project-f6d5c.appspot.com",
   messagingSenderId: "710690126374",
-  appId: process.env.FIREBASE_ID
+  appId: "1:710690126374:web:4dc7320ba092f23767c4e7"
 };
