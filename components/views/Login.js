d1baee49c8a1578974bfb628c8273221bcadf767
David Kemp <davkem43@gmail.com>
2020-04-13 16:52:13 -0500

    remove warning for autocomplete fields

--- components/views/Login.js
+++ components/views/Login.js
@@ -2,7 +2,7 @@ export default () => `
   <form id="Login-form" method="POST">
     <h3>Sign In</h3>
     <input type="text" placeholder="Email Address" id="user-email" required/>
-    <input type="password" placeholder="Password" id="user-pass" required/>
+    <input type="password" placeholder="Password" id="user-pass" autocomplete = "current-password" required/>
     <input type="submit" id="login" value="Sign In"/>
     <input type="submit" id="cancel" value="Cancel"/>
     <span class="psw">Forgot <a href="#">password?</a></span>
--- components/views/Register.js
+++ components/views/Register.js
@@ -4,8 +4,8 @@ export default () => `
 		<input type="text" placeholder="First Name" id="firstname" required/>
 		<input type="text" placeholder="Last Name" id="lastname" required/>
 		<input type="text" placeholder="Email Address" id="user-email" required/>
-		<input type="password" placeholder="Password" id="user-pass" required/>
-		<input type="password" placeholder="Verify Password" id="vuser-pass" required/>
+		<input type="password" placeholder="Password" id="user-pass" autocomplete = "current-password" required/>
+		<input type="password" placeholder="Verify Password" id="vuser-pass" autocomplete = "current-password" required/>
     <input type="submit" id="register" value="Register"/>
     <input type="submit" id="cancel" value="Cancel"/>
   </form>
